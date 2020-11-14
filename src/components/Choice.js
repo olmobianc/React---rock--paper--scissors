615e8f0fdbd604a32d557d60717b728c6318f957
olmo <olmo1995@gmail.com>
2020-11-14 17:40:00 +0100

    feat:added border colors through props

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -5,12 +5,16 @@ import './Choice.css';
 class Choice extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            borderColor: props.borderColor,
+            extraBorderColor: props.extraBorderColor
+        }
     }
     render() {
         return(
             <div>
-                <div className="elem-container">
-                    <img src={this.props.name} />
+                <div style={{borderColor: this.state.borderColor, color: this.state.extraBorderColor}} className="elem-container">
+                    <img src={this.props.image} />
                 </div>
             </div>
         )
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Choice from "./Choice"
+
 import paper from '../images/icon-paper.svg'
 import rock from '../images/icon-rock.svg'
 import scissors from '../images/icon-scissors.svg'
@@ -14,9 +15,21 @@ class Main extends React.Component {
         return(
             <div className="main-container">
                 <div className="triangle-container">
-                    <Choice name={paper} />
-                    <Choice name={scissors} />
-                    <Choice name={rock} />
+                    <Choice 
+                        image={paper} 
+                        borderColor="hsl(230, 89%, 62%)" 
+                        extraBorderColor="hsl(230, 89%, 65%)" 
+                    />
+                    <Choice 
+                        image={scissors} 
+                        borderColor="hsl(39, 89%, 49%)" 
+                        extraBorderColor="hsl(40, 84%, 53%)" 
+                    />
+                    <Choice 
+                        image={rock} 
+                        borderColor="hsl(349, 71%, 52%)" 
+                        extraBorderColor="hsl(349, 70%, 56%)" 
+                    />
                 </div>
             </div>
         )
