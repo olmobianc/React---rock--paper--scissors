22ca4467e0a277b60f37dd0fb0d14fc21647df6a
olmo <olmo1995@gmail.com>
2020-11-15 17:50:31 +0100

    feat:added modal pop up for the rules button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,40 @@
 import React from "react"
+
+import rulesImage from "../images/image-rules.svg"
+
 import './Footer.css';
 
-function Footer() {
-    return(
-        <div className="footer-box">
-            <div className="footer-container">
-                RULES
+var rulesBox = document.getElementById("rules-image-container")
+class Footer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isVisible: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState({
+            isVisible: true
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <div id="rules-image-conatinaer" style={{ display: (this.state.isVisible ? "block" : "none")}}>
+                    <h2 className="rules-title">RULES</h2>
+                    <img src={rulesImage} alt="rules"/>
+                </div>
+                <div className="footer-box">
+                    <div className="footer-container" onClick={this.handleClick}>
+                        RULES
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Footer
